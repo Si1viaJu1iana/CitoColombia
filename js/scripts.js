ee5cc13a8608d5e44a9be2dac97d868946fd1be4
Epilef07 <andresfelipepuentescarreno347@gmail.com>
2024-11-23 20:30:01 -0500

    CitoColombia

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+// Función para redirigir al formulario de registro
+function redirectToRegister() {
+    alert("Redirigiendo al formulario de registro...");
+    window.location.href = "register_user.html"; // Reemplazar con la URL del formulario
+}
---
+++ js/scripts2.js
@@ -0,0 +1,4 @@
+// Función para mostrar un mensaje emergente
+function showPopup() {
+    alert("CITOCOLOMBIA: Formación integral para un mundo mejor. Descubre más en nuestras secciones.");
+}
